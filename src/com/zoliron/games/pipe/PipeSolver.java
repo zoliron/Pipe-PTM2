2f26145d4a4eab18ef189e0f996abfff68c9761f
yaniv.zoicha <zolicha@gmail.com>
2018-07-23 21:52:10 +0300

    Update the PipeSolver

--- src/com/zoliron/games/pipe/PipeSolver.java
+++ src/com/zoliron/games/pipe/PipeSolver.java
@@ -6,6 +6,7 @@ import com.zoliron.games.pipe.searchable.PipeSearchableState;
 import com.zoliron.searcher.Solution;
 import com.zoliron.searcher.algorithms.HillClimbing;
 import com.zoliron.solver.Solver;
+import com.zoliron.utils.Point;
 
 
 
@@ -31,9 +32,50 @@ public class PipeSolver implements Solver<PipeBoard>{
 
 		Solution<PipeSearchableState> solution = new HillClimbing<PipeSearchableState>().search(searchable);
 
-		String s = "dfgdsbg";
+		int[][] clickCount = countClicks(problem, solution);
 
-		return s;
+		return toString(problem, clickCount);
+	}
+
+
+
+	/**
+	 * Count the number of clicks on each cell of the specified {@link PipeBoard} according to the given solution.
+	 */
+	private int[][] countClicks(PipeBoard problem, Solution<PipeSearchableState> solution){
+		int rowCount = problem.getRowCount();
+		int columnCount = problem.getColumnCount();
+		int[][] result = new int[rowCount][columnCount];
+
+		for (PipeSearchableState state : solution.getStates()){
+			Point action = state.getAction();
+			int column = action.x;
+			int row = action.y;
+
+			result[row][column]++;
+		}
+
+		return result;
+	}
+
+
+
+	/**
+	 * Returns the click solution string of the specified clicks count matrix.
+	 */
+	private String toString(PipeBoard problem, int[][] clickCount){
+		int rowCount = problem.getRowCount();
+		int columnCount = problem.getColumnCount();
+
+		StringBuilder sb = new StringBuilder();
+		for (int row = 0; row < rowCount; row++){
+			for (int column = 0; column < columnCount; column++){
+				sb.append(row).append(',').append(column).append(',').append(clickCount[row][column]).append('\n');
+			}
+		}
+
+		sb.append("done");
+		return sb.toString();
 	}
 
 
